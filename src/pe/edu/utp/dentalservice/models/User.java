a1e4d3d7df6875a934f5ae5381d7d6a816a673cc
saragomezorihuela <alexcesaraguilar@gmail.com>
2017-02-24 18:39:30 -0500

    Controlador de usuario

---
+++ src/pe/edu/utp/dentalservice/models/User.java
@@ -0,0 +1,14 @@
+package pe.edu.utp.dentalservice.models;
+
+/**
+ * Created by Cadellas on 24/02/2017.
+ */
+public class User {
+
+    private int idUser;
+    private String descriptionUser;
+
+    public User() {
+    }
+
+}
