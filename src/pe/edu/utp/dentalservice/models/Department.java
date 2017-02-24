1bb2aa6d1a09a2604e3efb2d4e3efc9278cc160c
saragomezorihuela <urtedrox44@gmail.com>
2017-02-24 18:33:38 -0500

    Controlador de provincia

--- src/pe/edu/utp/dentalservice/models/Department.java
+++ src/pe/edu/utp/dentalservice/models/Department.java
@@ -4,4 +4,11 @@ package pe.edu.utp.dentalservice.models;
  * Created by jayrivarez on 24/02/2017.
  */
 public class Department {
+
+    private int idDepartment;
+    private String descriptionDepartment;
+
+    public Department() {
+    }
+
 }
--- src/pe/edu/utp/dentalservice/models/Province.java
+++ src/pe/edu/utp/dentalservice/models/Province.java
@@ -4,4 +4,11 @@ package pe.edu.utp.dentalservice.models;
  * Created by jayrivarez on 24/02/2017.
  */
 public class Province {
+
+    private int idProvince;
+    private String descriptionProvince;
+
+    public Province() {
+    }
+
 }
