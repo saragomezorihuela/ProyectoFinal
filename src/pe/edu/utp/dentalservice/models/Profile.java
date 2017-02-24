e9f5a2d5a5bf15f3bc44ff6666365b114fe5b5d9
saragomezorihuela <alexcesaraguilar@gmail.com>
2017-02-24 18:38:43 -0500

    Controlador de perfil

--- src/pe/edu/utp/dentalservice/models/Profile.java
+++ src/pe/edu/utp/dentalservice/models/Profile.java
@@ -4,4 +4,11 @@ package pe.edu.utp.dentalservice.models;
  * Created by Cadellas on 24/02/2017.
  */
 public class Profile {
+
+    private int idProfile;
+    private String descriptionProfile;
+
+    public Profile() {
+    }
+
 }
