414864d9f614a069fa258cd0bb3a431af42fb4b4
saragomezorihuela <1221891@utp.edu.pe>
2017-02-25 12:03:33 -0500

    Modificado por Sara Sheena

---
+++ src/pe/edu/utp/dentalservice/models/PersonEntity.java
@@ -0,0 +1,14 @@
+package pe.edu.utp.dentalservice.models;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sara Sheena on 25/02/2017.
+ */
+public class PersonEntity extends BaseEntity{
+
+}
