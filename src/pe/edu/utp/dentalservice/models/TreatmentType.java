88d67f0363b9832fd8967e40f4f559daff47cff4
1311543 <1311543@utp.edu.pe>
2017-03-04 13:32:35 -0500

    Creación de la entidad TREATMENT TYPE

---
+++ src/pe/edu/utp/dentalservice/models/TreatmentType.java
@@ -0,0 +1,51 @@
+package pe.edu.utp.dentalservice.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ * Created by DE LA CRUZ on 04/03/2017.
+ */
+public class TreatmentType {
+
+    private int id;
+    private String description;
+
+    public TreatmentType(){}
+
+    public TreatmentType(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public TreatmentType setId(int id){
+        this.id = id;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TreatmentType setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public static IdentityCard build(ResultSet resultSet) {
+        try {
+            return new IdentityCard(resultSet.getInt("id"),
+                    resultSet.getString("description"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+}
