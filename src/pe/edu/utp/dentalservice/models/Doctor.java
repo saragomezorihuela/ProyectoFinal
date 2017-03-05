8da3c27ed63723417d06262e2a0a3fcee81d348f
saragomezorihuela <1221891@utp.edu.pe>
2017-03-05 10:51:10 -0500

    Creación de la entidad DOCTOR

--- src/pe/edu/utp/dentalservice/models/Doctor.java
+++ src/pe/edu/utp/dentalservice/models/Doctor.java
@@ -1,5 +1,9 @@
 package pe.edu.utp.dentalservice.models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
 /**
  * Created by SARA SHEENA on 24/02/2017.
  */
@@ -7,12 +11,16 @@ public class Doctor extends Person {
 
     private String CCO;
 
-    public Doctor(String CCO) {
-        this.CCO = CCO;
+    public Doctor() {
+        super();
     }
 
-    public Doctor() {
-        
+    public Doctor(int id, String dni, String firstname, String lastname, Date birthdate, String phone, String cellphone, String email, String CCO,
+                  Hospital hospital, Gender gender, BloodType bloodType, IdentityCard identityCard) {
+
+        super(id, dni, firstname, lastname, birthdate, phone, cellphone, email, hospital, gender, bloodType, identityCard);
+        this.CCO = CCO;
+
     }
 
     public String getCCO() {
@@ -24,4 +32,26 @@ public class Doctor extends Person {
         return this;
     }
 
+    public static Doctor build(ResultSet resultSet, HospitalsEntity hospitalEntity, GendersEntity gendersEntity,
+                               BloodTypesEntity bloodTypesEntity, IdentityCardsEntity identityCardsEntity) {
+        try {
+            return new Doctor(resultSet.getInt("id"),
+                            resultSet.getString("dni"),
+                            resultSet.getString("first_name"),
+                            resultSet.getString("last_name"),
+                            resultSet.getDate("birth_date"),
+                            resultSet.getString("phone"),
+                            resultSet.getString("cellphone"),
+                            resultSet.getString("email"),
+                            resultSet.getString("CCO"),
+                            hospitalEntity.findById(resultSet.getInt("hospital_id")),
+                                    gendersEntity.findById(resultSet.getInt("gender_id")),
+                                            bloodTypesEntity.findById(resultSet.getInt("blood_type_id")),
+                                                    identityCardsEntity.findById(resultSet.getInt("identity_card_id"))
+                    );
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
