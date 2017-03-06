4cf8c1e7f8a60e69b38f5ab8ce331bb7c3ad88f7
Cadellas <alexcesaraguilar@gmail.com>
2017-03-06 12:51:53 -0500

    Creación DTO para DISTRICTS Realiza la selección, inserción, actualización y eliminación de datos.

---
+++ src/pe/edu/utp/dentalservice/models/DistrictsEntity.java
@@ -0,0 +1,77 @@
+package pe.edu.utp.dentalservice.models;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Alex Aguilar  on 06/03/2017.
+ */
+public class DistrictsEntity extends BaseEntity {
+
+
+    public DistrictsEntity() {
+        super("DISTRICTS");
+    }
+
+    public List<District> findAll() {
+        String statement = getDefaultStatement() + getTableName();
+        return findByCriteria(statement);
+    }
+
+    private List<District> findByCriteria(String sql) {
+        List<District> districts = new ArrayList<>();
+        try {
+            ResultSet rs = getConnection().createStatement().executeQuery(sql);
+            while(rs.next()) {
+                District district = District.build(rs);
+                districts.add(district);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return districts;
+    }
+
+    public District findById(int id) {
+        String statement = "SELECT * FROM DISTRICTS WHERE id = " +
+                String.valueOf(id);
+        List<District> districts = findByCriteria(statement);
+        return districts != null ? districts.get(0) : null;
+    }
+
+    public District findByName(String name) {
+        String statement = "SELECT * FROM DISTRICTS WHERE description = '" +
+                name + "'";
+        List<District> districts = findByCriteria(statement);
+        return districts != null ? districts.get(0) : null;
+    }
+
+    private int updateByCriteria(String sql) {
+        try {
+            return getConnection().createStatement().executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public District create(int id, String description) {
+        String sql = "INSERT INTO DISTRICTS(id, description) " +
+                "VALUES(" + String.valueOf(id) + ", '" + description + "')";
+        return updateByCriteria(sql) > 0 ? new District(id, description) : null;
+    }
+
+    public boolean update(District district) {
+        String sql = "UPDATE DISTRICTS SET description = '" + district.getDescription() + "," +
+                "' WHERE id = " + String.valueOf(district.getId());
+        return updateByCriteria(sql) > 0;
+    }
+
+    public boolean delete(int id) {
+        String sql = "DELETE FROM DISTRICTS WHERE id = " + String.valueOf(id);
+        return updateByCriteria(sql) > 0;
+    }
+
+}
