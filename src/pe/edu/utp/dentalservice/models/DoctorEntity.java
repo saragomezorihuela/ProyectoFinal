4dee14eb5bc3f32bb1f6d31dfc5e37ecd6ecf06a
saragomezorihuela <1221891@utp.edu.pe>
2017-03-07 14:27:40 -0500

    Creación DTO para DOCTORS Realiza la selección, inserción, actualización y eliminación de datos.

---
+++ src/pe/edu/utp/dentalservice/models/DoctorEntity.java
@@ -0,0 +1,18 @@
+package pe.edu.utp.dentalservice.models;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Sara Sheena on 28/02/2017.
+ */
+public class DoctorEntity  extends BaseEntity{
+
+    public DoctorEntity() {
+        super("doctor");
+    }
+
+}
