b419af396a1f18a20c1fc6bcebcc2e0edd55610d
Jonaae <jonaae1@gmail.com>
2017-03-07 14:05:19 -0500

    Creación de la entidad TOOTH.

--- src/pe/edu/utp/dentalservice/models/Tooth.java
+++ src/pe/edu/utp/dentalservice/models/Tooth.java
@@ -1,37 +1,63 @@
 package pe.edu.utp.dentalservice.models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by De la Cruz on 25/02/2017.
  */
 public class Tooth {
 
-    private int idTooth;
-    private String descriptionTooth;
+    private int id;
+    private int number;
+    private String description;
 
-    public Tooth(int idTooth, String descripcionTooth) {
-        this.idTooth = idTooth;
-        this.descriptionTooth = descriptionTooth;
+    public Tooth(int id, int number, String description) {
+        this.id = id;
+        this.description = description;
+        this.number = number;
     }
 
     public Tooth() {
     }
 
-    public int getIdTooth() {
-        return idTooth;
+    public int getId() {
+        return id;
     }
 
-    public Tooth setTooth(int idTooth) {
-        this.idTooth = idTooth;
+    public Tooth setId(int id) {
+        this.id = id;
         return this;
     }
 
-    public String getDescripcionTreatment() {
-        return descriptionTooth;
+    public int getNumber() {
+        return number;
     }
 
-    public Tooth setDescripcionTypeTreatment(String descripcionTooth) {
-        this.descriptionTooth = descripcionTooth;
+    public Tooth setNumber(int number) {
+        this.number = number;
         return this;
     }
-    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Tooth setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public static Tooth build(ResultSet resultSet) {
+        try {
+            return new Tooth(resultSet.getInt("id"),
+                    resultSet.getInt("number"),
+                    resultSet.getString("description"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
 }
