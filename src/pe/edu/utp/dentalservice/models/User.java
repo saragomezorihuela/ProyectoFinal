a290ed7d2463ec1ba250ae5ba74501079a0c43f9
jayrivarez <urtedrox44@gmail.com>
2017-03-07 14:25:22 -0500

    Creación de la entidad USER

--- src/pe/edu/utp/dentalservice/models/User.java
+++ src/pe/edu/utp/dentalservice/models/User.java
@@ -1,38 +1,38 @@
 package pe.edu.utp.dentalservice.models;
 
 /**
- * Created by Alex Aguillar on 24/02/2017.
+ * Created by Rodrigo Rivas on 24/02/2017.
  */
 public class User {
 
-    private int idUser;
-    private String descriptionUser;
+    private int id;
+    private String description;
 
-    public User(int idUser, String descriptionUser) {
+    public User(int id, String description) {
 
-        this.idUser = idUser;
-        this.descriptionUser = descriptionUser;
+        this.id = id;
+        this.description = description;
 
     }
 
     public User() {
     }
 
-    public int getIdUser() {
-        return idUser;
+    public int getId() {
+        return id;
     }
 
-    public User setIdUser(int idUser) {
-        this.idUser = idUser;
+    public User setId(int id) {
+        this.id = id;
         return this;
     }
 
-    public String getDescriptionUser() {
-        return descriptionUser;
+    public String getDescription() {
+        return description;
     }
 
-    public User setDescriptionUser(String descriptionUser) {
-        this.descriptionUser = descriptionUser;
+    public User setDescription(String description) {
+        this.description = description;
         return this;
     }
 
