37eaddcb4739a5e968e28cfe06b8ba99a9b8fca6
jayrivarez <urtedrox44@gmail.com>
2017-03-11 06:57:18 -0500

    Creación de la entidad USERS

--- src/pe/edu/utp/dentalservice/models/User.java
+++ src/pe/edu/utp/dentalservice/models/User.java
@@ -1,17 +1,30 @@
 package pe.edu.utp.dentalservice.models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created by Rodrigo Rivas on 24/02/2017.
  */
 public class User {
 
     private int id;
-    private String description;
+    private String name;
+    private String password;
+    private String state;
 
-    public User(int id, String description) {
+    private Profile profile;
+    private Person person;
+
+    public User(int id, String name, String password, String state, Profile profile) {
 
         this.id = id;
-        this.description = description;
+        this.name = name;
+        this.password = password;
+        this.state = state;
+
+        //this.person = person;
+        this.profile = profile;
 
     }
 
@@ -27,13 +40,60 @@ public class User {
         return this;
     }
 
-    public String getDescription() {
-        return description;
+    public String getName() {
+        return name;
     }
 
-    public User setDescription(String description) {
-        this.description = description;
+    public User setName(String name) {
+        this.name = name;
         return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public User setSate(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public static User build(ResultSet resultSet, ProfilesEntity profileEntity) {
+        try {
+            return new User(resultSet.getInt("id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("password"),
+                    resultSet.getString("state"),
+                    profileEntity.findById(resultSet.getInt("profile_id")));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
