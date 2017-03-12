306863df0c086f48627f7c01e698db380684662c
Jonaae <jonaae1@gmail.com>
2017-03-12 09:56:16 -0500

    Creación DTO para APPOINTMENT Realiza la selección, inserción, actualización y eliminación de datos.

---
+++ src/pe/edu/utp/dentalservice/models/AppointmentsEntity.java
@@ -0,0 +1,18 @@
+package pe.edu.utp.dentalservice.models;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jonathan Rojas on 12/03/2017.
+ */
+public class AppointmentsEntity extends BaseEntity{
+
+    public AppointmentsEntity() {
+        super("APPOINTMENTS");
+    }
+
+}
