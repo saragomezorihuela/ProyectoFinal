6ea525ce0fb3d2fe318bee0c3e0a9ca15bb4f2b0
1311543 <1311543@utp.edu.pe>
2017-03-14 08:00:34 -0500

    Creación de la entidad TREATMENT

--- src/pe/edu/utp/dentalservice/models/Treatment.java
+++ src/pe/edu/utp/dentalservice/models/Treatment.java
@@ -1,36 +1,87 @@
 package pe.edu.utp.dentalservice.models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
- * Created by De la Cruz on 25/02/2017.
+ * Created by DE LA CRUZ on 14/03/2017.
  */
 public class Treatment {
 
-    private int idTreatment;
-    private String descriptionTreatment;
+    private int id;
+    private String description;
+    private float cost;
+    private int sessionCount;
 
-    public Treatment(int idTreatment, String descripcionTreatment) {
-        this.idTreatment = idTreatment;
-        this.descriptionTreatment = descriptionTreatment;
+    private TreatmentType treatmentType;
+
+    public Treatment(int id, String description, float cost, int sessionCount, TreatmentType treatmentType){
+
+        this.id = id;
+        this.description = description;
+        this.cost = cost;
+        this.sessionCount = sessionCount;
+
+        this.treatmentType = treatmentType;
+
     }
 
     public Treatment() {
     }
 
-    public int getIdTreatment() {
-        return idTreatment;
+    public int getId() {
+        return id;
     }
 
-    public Treatment setIdTreatment(int idTreatment) {
-        this.idTreatment = idTreatment;
+    public Treatment setId(int id) {
+        this.id = id;
         return this;
     }
 
-    public String getDescripcionTreatment() {
-        return descriptionTreatment;
+    public String getDescription() {
+        return description;
     }
 
-    public Treatment setDescripcionTypeTreatment(String descripcionTreatment) {
-        this.descriptionTreatment = descripcionTreatment;
+    public Treatment setDescription(String description) {
+        this.description = description;
         return this;
     }
+
+    public float getCost(){return cost;}
+
+    public Treatment setCost(float cost){
+        this.cost = cost;
+        return this;
+    }
+
+    public int getSessionCount() {
+        return sessionCount;
+    }
+
+    public Treatment setSessionCount(int sessionCount) {
+        this.sessionCount = sessionCount;
+        return this;
+    }
+
+    public TreatmentType getTreatmentType() {
+        return treatmentType;
+    }
+
+    public void setTreatmentType(TreatmentType treatmentType) {
+        this.treatmentType = treatmentType;
+    }
+
+    public static Treatment build(ResultSet resultSet, TreatmentTypesEntity treatmentTypesEntity) {
+        try {
+            return new Treatment(resultSet.getInt("id"),
+                    resultSet.getString("description"),
+                    resultSet.getFloat("cost"),
+                    resultSet.getInt("sessioncount"),
+                    treatmentTypesEntity.findById(resultSet.getInt("type_treatment_id")));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
