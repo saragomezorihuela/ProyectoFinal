588c5783b620cd31c8395f62cf66c45834a2fe02
saragomezorihuela <1221891@utp.edu.pe>
2017-03-23 11:56:23 -0500

    Creación de la entidad PATIENT

--- src/pe/edu/utp/dentalservice/models/Patient.java
+++ src/pe/edu/utp/dentalservice/models/Patient.java
@@ -1,16 +1,31 @@
 package pe.edu.utp.dentalservice.models;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.Date;
+
 /**
  * Created by SARA SHEENA on 24/02/2017.
  */
-public class Patient extends Person {
+@ManagedBean
+@RequestScoped
+public class Patient extends People {
 
-    private int idPatient;
+    private int id;
     private String relativename;
     private String relativephone;
 
-    public Patient(int idPatient, String relativename, String relativephone) {
-        this.idPatient = idPatient;
+    public Patient(int id, String nroDocumento, String firstname, String lastname, Date birthdate,
+                   String phone, String cellphone, String email, String relativename, String relativephone) {
+
+        this.id = id;
+        this.nroDocumento = nroDocumento;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.phone = phone;
+        this.cellphone = cellphone;
+        this.email = email;
         this.relativename = relativename;
         this.relativephone = relativephone;
     }
@@ -18,30 +33,20 @@ public class Patient extends Person {
     public Patient() {
     }
 
-    public int getIdPatient() {
-        return idPatient;
-    }
-
-    public Patient setIdPatient(int idPatient) {
-        this.idPatient = idPatient;
-        return this;
-    }
-
     public String getRelativename() {
         return relativename;
     }
 
-    public Patient setRelativename(String relativename) {
+    public void setRelativename(String relativename) {
         this.relativename = relativename;
-        return this;
     }
 
     public String getRelativephone() {
         return relativephone;
     }
 
-    public Patient setRelativephone(String relativephone) {
+    public void setRelativephone(String relativephone) {
         this.relativephone = relativephone;
-        return this;
     }
+
 }
