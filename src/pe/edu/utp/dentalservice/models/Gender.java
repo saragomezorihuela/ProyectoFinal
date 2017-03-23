9c7b0d1e51617e78bb7b56c7407afb55c7e55ded
saragomezorihuela <1221891@utp.edu.pe>
2017-03-23 07:26:41 -0500

    Creación de la entidad GENERO

--- src/pe/edu/utp/dentalservice/models/Gender.java
+++ src/pe/edu/utp/dentalservice/models/Gender.java
@@ -1,53 +1,41 @@
 package pe.edu.utp.dentalservice.models;
 
-import com.sun.org.apache.xpath.internal.operations.String;
-
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
  * Created by SARA SHEENA on 04/03/2017.
  */
+@ManagedBean
+@RequestScoped
 public class Gender {
 
     private int id;
     private String description;
-
-    //VERIFICAR
-    public Gender(int id, java.lang.String description){}
 
     public Gender(int id, String description) {
         this.id = id;
         this.description = description;
     }
 
+    public Gender(){}
+
     public int getId(){
         return id;
     }
 
-    public Gender setId(int id){
+    public void setId(int id){
         this.id = id;
-        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public Gender setDescription(String description) {
+    public void setDescription(String description) {
         this.description = description;
-        return this;
-    }
-
-    public static Gender build(ResultSet resultSet) {
-        try {
-            return new Gender(resultSet.getInt("id"),
-                    resultSet.getString("description"));
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-
     }
 
 }
