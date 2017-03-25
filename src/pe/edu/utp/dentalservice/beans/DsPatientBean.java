debbc76492e48be666d5e708b3e28089284fa1e9
saragomezorihuela <1221891@utp.edu.pe>
2017-03-25 03:18:39 -0500

    Bean que permite interactuar con la información de PATIENT

---
+++ src/pe/edu/utp/dentalservice/beans/DsPatientBean.java
@@ -0,0 +1,80 @@
+package pe.edu.utp.dentalservice.beans;
+
+import pe.edu.utp.dentalservice.models.*;
+import pe.edu.utp.dentalservice.service.DsService;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by Sara Sheena on 19/03/2017.
+ */
+@ManagedBean(name = "patientBean", eager = true)
+@SessionScoped
+public class DsPatientBean {
+
+    private DsService service;
+
+    private Patient patient = new Patient();
+    private People people = new People();
+
+    public DsPatientBean() {
+        try {
+            InitialContext ctx = new InitialContext();
+            Connection connection = ((DataSource) ctx
+                    .lookup("jdbc/MySQLDataSourceDs"))
+                    .getConnection();
+            service = new DsService();
+            service.setConnection(connection);
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getPatientsCount() {
+        return service.getPatientsCount();
+    }
+
+    public List<Patient> getPatients() { return service.findAllPatients(); }
+
+    public String add(People people, Patient patient, IdentityCard identityCard, Gender gender) {
+
+        service.addPatient(people, patient, identityCard, gender);
+        return "PatientList";
+    }
+
+    public String update(Patient patient) {
+         service.updatePatient(patient);
+         return "PatientList";
+    }
+
+    public boolean delete(Patient doctor) { return service.deletePatient(patient); }
+
+    public String showPatientsCount() {
+        if(getPatientsCount() > 0) {
+            return "success";
+        } else {
+            return "error";
+        }
+    }
+
+    public Patient getSelectedPatient() {
+        return patient;
+    }
+
+    public String edit(Patient patient) {
+        this.patient = patient;
+        return "PatientEdit";
+    }
+
+    public String listPatients() {
+        return "success";
+    }
+
+}
