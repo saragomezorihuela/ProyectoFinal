87dc2277415af69b38dc388b8c66041ca3fde51c
Cadellas <alexcesaraguilar@gmail.com>
2017-03-25 03:39:48 -0500

    Creación de la entidad DEPARTMENT

--- src/pe/edu/utp/dentalservice/models/Department.java
+++ src/pe/edu/utp/dentalservice/models/Department.java
@@ -1,11 +1,15 @@
 package pe.edu.utp.dentalservice.models;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
  * Created by Alex Aguilar on 24/02/2017.
  */
+@ManagedBean
+@RequestScoped
 public class Department {
 
     private int id;
@@ -13,11 +17,9 @@ public class Department {
 
     private Country country;
 
-    public Department(int id, String description, Country country) {
+    public Department(int id, String description) {
         this.id = id;
         this.description = description;
-
-        this.country = country;
     }
 
     public Department() {
@@ -27,18 +29,16 @@ public class Department {
         return id;
     }
 
-    public Department setId(int id) {
+    public void setId(int id) {
         this.id = id;
-        return this;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public Department setDescription(String description) {
+    public void setDescription(String description) {
         this.description = description;
-        return this;
     }
 
     public Country getCountry() {
@@ -47,17 +47,6 @@ public class Department {
 
     public void setCountry(Country country) {
         this.country = country;
-    }
-
-    public static Department build(ResultSet resultSet, CountriesEntity countriesEntity) {
-        try {
-            return new Department(resultSet.getInt("id"),
-                    resultSet.getString("description"),
-                    countriesEntity.findById(resultSet.getInt("country_id")));
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 
 }
